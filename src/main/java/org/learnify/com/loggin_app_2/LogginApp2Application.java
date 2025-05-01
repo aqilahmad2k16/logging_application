5227863376faae1480f7c724644e00983723a95b
aqilahmad2k16 <er.aqilahmad031@gmail.com>
2025-05-01 23:03:45 +0530

    I have setup the loggin application from scratch

---
+++ src/main/java/org/learnify/com/loggin_app_2/LogginApp2Application.java
@@ -0,0 +1,13 @@
+package org.learnify.com.loggin_app_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogginApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogginApp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/learnify/com/loggin_app_2/LogginApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learnify.com.loggin_app_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogginApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
