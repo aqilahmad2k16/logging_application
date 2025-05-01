8be604915b15a9ae7ef5a235f5cbffc185bb9a30
aqilahmad2k16 <er.aqilahmad031@gmail.com>
2025-05-01 23:09:51 +0530

    added LogController to the controller directory

---
+++ src/main/java/org/learnify/com/loggin_app_2/controllers/LogController.java
@@ -0,0 +1,9 @@
+package org.learnify.com.loggin_app_2.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/logging/v1/api")
+public class LogController {
+}
